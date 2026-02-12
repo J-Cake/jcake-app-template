8706c6cafefb698b4a9bb09ae1f654d485e3dc5e
jcake <jakieschneider13@gmail.com>
2026-02-12 23:25:52 +0100

    Let's also make sure the blank project compiles and runs

--- app/status.tsx
+++ app/status.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import {API} from "./main";
-import {Awaited} from "./util";
 
-export default function StatusBar(props: {}) {
-	const api = React.useContext(API);
-
+export default function StatusBar() {
 	return <div id="status">
 		<div id="status-left"></div>
 		<div id="status-centre"></div>
 		<div id="status-right">
-			<a href="#">
-				<Awaited promise={api.version()}>
-					{version => `v${version.version}`}
-				</Awaited>
-			</a>
+
 		</div>
 	</div>
 }
